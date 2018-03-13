73e8fec9b283b659aa94f07582e82893aa965227
Despoina Tosounidou <despoina.tosounidou@gmail.com>
2018-03-13 19:39:12 +0200

    ...

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -2,12 +2,12 @@
   import * as firebase from 'firebase';
 
   var config = {
-    apiKey: "AIzaSyCtDkhwYizmBtGYXLlObscQIyDhP_ILbpo",
-    authDomain: "crm-react-app.firebaseapp.com",
-    databaseURL: "https://crm-react-app.firebaseio.com",
-    projectId: "crm-react-app",
-    storageBucket: "crm-react-app.appspot.com",
-    messagingSenderId: "62755360527"
+    // apiKey: "AIzaSyCtDkhwYizmBtGYXLlObscQIyDhP_ILbpo",
+    // authDomain: "crm-react-app.firebaseapp.com",
+    // databaseURL: "https://crm-react-app.firebaseio.com",
+    // projectId: "crm-react-app",
+    // storageBucket: "crm-react-app.appspot.com",
+    // messagingSenderId: "62755360527"
   };
 
   if (!firebase.apps.length) {
